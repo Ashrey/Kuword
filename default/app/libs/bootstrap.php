ba40a3fafc7e8e8ad59a7ff97116022b9c4ae36f
Alberto Berroteran <aberroteran@live.com>
2014-10-18 17:16:44 -0430

    New autoload

--- default/app/libs/bootstrap.php
+++ default/app/libs/bootstrap.php
@@ -3,8 +3,7 @@
 // Para cargar cambia en public/index.php el require del bootstrap a app
 
 // Arranca KumbiaPHP
-$vendor = dirname(dirname(dirname(__DIR__))).'/vendor/';
-require "$vendor/parsedown/Parsedown.php";
+require dirname(dirname(dirname(__DIR__))).'/vendor/autoload.php';
 require dirname(dirname(dirname(__DIR__))).'/backend/app/libs/autoload.php';
 require_once CORE_PATH . 'kumbia/bootstrap.php';
 
