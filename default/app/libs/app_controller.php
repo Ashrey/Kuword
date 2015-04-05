52cf3ff4cecf053f51aec1d2dfec2250d01774bf
Alberto Berroteran <aberroteran@live.com>
2015-04-05 15:27:01 -0430

    Remove comment

--- default/app/libs/app_controller.php
+++ default/app/libs/app_controller.php
@@ -1,38 +1,16 @@
 <?php
-/**
- * Controlador principal que heredan los controladores
- *
- * Todas las controladores heredan de esta clase en un nivel superior
- * por lo tanto los metodos aqui definidos estan disponibles para
- * cualquier controlador.
- *
- * @category Kumbia
- * @package Controller
- **/
-
-/**
- * @see Controller nuevo controller
- */
-require_once CORE_PATH . 'kumbia/controller.php';
-
-function __(){
-	return call_user_func_array(array('I18n', 'get'), func_get_args());
-}
 Load::models('posts');
-class AppController extends  \KBackend\Libs\ScaffoldController {
+class AppController extends \KBackend\Libs\ScaffoldController {
 
-	final protected function initialize()
-	{
-        
+	final protected function initialize() {
+
 	}
 
-	final protected function finalize()
-	{
-        $this->pagename  = Conf::get('title');
-        $this->pagedesc   = Conf::get('desc');
-        $this->style  = Conf::get('style').'/'.Conf::get('style');
-        $this->footer = Posts::getSection('footer');
-        $this->ptitle = isset($this->ptitle)?$this->ptitle:$this->title;
-
+	final protected function finalize() {
+		$this->pagename = Conf::get('title');
+		$this->pagedesc = Conf::get('desc');
+		$this->style = Conf::get('style') . '/' . Conf::get('style');
+		$this->footer = Posts::getSection('footer');
+		$this->ptitle = isset($this->ptitle) ? $this->ptitle : $this->title;
 	}
 }
